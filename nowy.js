19a16bf75248e6d0605985953860868bd1403fee
wsGit7 <137900710+wsGit7@users.noreply.github.com>
2024-12-27 19:07:35 +0100

    nowy.js

--- nowy.js
+++ nowy.js
@@ -1,49 +1,45 @@
 document.getElementById('myfile').addEventListener('change', function() {
-
-
   var GetFile = new FileReader();
+  let fetchController;
+  
+  GetFile.onload = function() {
+    var Zosia = GetFile.result.split('.');
+    let anna = document.getElementById("msg").value;
+    let basia = document.getElementById("msg2").value;
+    let i = parseInt(anna, 10);
 
-   GetFile .onload=function(){
-        
-  var Zosia = GetFile.result.split('.');
-  // alert(Zosia.length);
-  var i = 0;
-  let anna = document.getElementById("msg").value;
-  let basia = document.getElementById("msg2").value;
+    const timer = ms => new Promise(res => setTimeout(res, ms));
 
-  const timer = ms => new Promise(res => setTimeout(res, ms))
-
-  async function load () { // We need to wrap the loop into an async function for this to work
-    for (var i = anna; i < Zosia.length; i++) {
-      document.getElementById('output').innerHTML= Zosia[i];
-      document.getElementById('output2').innerHTML= "("+i+"/"+Zosia.length+" =  "+((i/Zosia.length)*100).toFixed(2)+" % )";
-      // document.getElementById('output3').innerHTML= basia;
-      // document.getElementById('output').value= Zosia[i];
-      
-      let rafal = Zosia[i].length/20;
-      await timer(basia*1000*rafal); // then the created Promise can be awaited
+    async function load() {
+      for (; i < Zosia.length; i++) {
+        if (fetchController && fetchController.signal.aborted) {
+          break;
+        }
+        document.getElementById('output').innerHTML = Zosia[i];
+        document.getElementById('output2').innerHTML = `(${i}/${Zosia.length} = ${(i / Zosia.length * 100).toFixed(2)} % )`;
+        let rafal = Zosia[i].length / 20;
+        await timer(basia * 1000 * rafal); // then the created Promise can be awaited
+      }
     }
+
+    fetchController = new AbortController();
+    load();
+
+    document.getElementById('stopButton').addEventListener('click', () => {
+      if (fetchController) {
+        fetchController.abort();
+        document.getElementById('stopButton').disabled = true;
+        document.getElementById('resumeButton').disabled = false;
+      }
+    });
+
+    document.getElementById('resumeButton').addEventListener('click', () => {
+      fetchController = new AbortController();
+      load();
+      document.getElementById('stopButton').disabled = false;
+      document.getElementById('resumeButton').disabled = true;
+    });
   }
-  
-  load();
-  
-    
 
-
-
-
-
-
-  
-
-  
-  
-}
-
-    
-    GetFile.readAsText(this.files[0]);
-})
-
-
-
- 
+  GetFile.readAsText(this.files[0]);
+});
